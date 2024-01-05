f1196a3cd9f3e456e91f7515effe5b85d4f65b76
Disha Revandkar <55402567+DishaRevandkar@users.noreply.github.com>
2024-01-05 06:01:02 -0500

    Update OverlapException.java

--- src/battleship/OverlapException.java
+++ src/battleship/OverlapException.java
@@ -4,8 +4,7 @@ package battleship;
  * A BattleshipException that informs the program that it attempted
  * to place a ship where there is already another ship
  *
- * @author Shreya Pramod    sp3045@rit.edu
- * @author Disha Revandkar  dr6742@rit.edu
+ * @author Disha Revandkar  
  */
 public class OverlapException extends BattleshipException{
     /**
